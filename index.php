0fb57fd449d21cac27c47a2238b45fc7f786f012
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-17 18:31:53 -0300

    criação da tabela de dados

--- index.php
+++ index.php
@@ -14,6 +14,60 @@
             <h2>Lorem Ipsum Inc</h2>
             <a href=""><button><i class="fa fa-plus"></i>Adicionar projeto</button></a>
         </div>
+    <?php
+    //  incluindo o arquivo config.php
+        require_once "config.php";
+    // Tentar selecionar a execução da consulta
+        $sql = "SELECT * FROM listaprojetos"; 
+        if ($result = mysqli_query($link, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
+                echo '<"table class="tabela">';
+                    echo "<theade>";
+                        echo '<tr>';
+                            echo "<th>#</th>";
+                            echo "<th>Nome do Projeto</th>";
+                            echo "<th>Inicio do projeto</th>";
+                            echo "<th>Fim do projeto</th>";
+                            echo "<th>Valor do Projeto</th>";
+                            echo "<th>Riscos do projeto</th>";
+                            echo "<th>Participantes</th>";
+                            echo "<th>Ação</th>";
+                        echo "</tr>";
+                        echo "</thead>";
+                        echo "<tbody>";
+                        while($row = mysqli_fetch_array($result)){
+                                echo "<tr>";
+                                    echo "<td>" . $row['id'] . "</td>"; 
+                                    echo "<td>" . $row['nome'] . "</td>"; 
+                                    echo "<td>" . $row['inicio'] . "</td>"; 
+                                    echo "<td>" . $row['fim'] . "</td>"; 
+                                    echo "<td>" . $row['valor'] . "</td>"; 
+                                    echo "<td>" . $row['riscos'] . "</td>"; 
+                                    echo "<td>" . $row['participantes'] . "</td>";
+                                    echo "<td>";
+                                    echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                    echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                    echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                                        echo "<td>";
+                                                    echo "<tr>";
+                                                }
+                                                echo "</tbody>";
+                                            echo "</table>";
+
+                                        // resultados
+                                        mysqli_free_result($result);
+                        } else {
+                            echo '<div class="alerta"> <em>Nenhum registro foi encontrado.</em></div>';
+                        }
+            }else {
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+                //fechar conexão
+                mysqli_close($link);
+            ?>
+    </div>
+            </div>        
+        </div>
     </div>
 </body>
 </html>
