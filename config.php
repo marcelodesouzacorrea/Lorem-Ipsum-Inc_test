52ef031434529e739575ad336e6d1906ccd453e7
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-17 16:00:47 -0300

    credenciais do banco de dados

---
+++ config.php
@@ -0,0 +1,9 @@
+
+<!-- Credenciais do banco de dados. Supondo que você esteja executando o MySQL
+servidor com configuração padrão (usuário 'root' sem senha) -->
+<?php
+    define('DB_SERVER','localhost');
+    define('DB_USER','root');
+    define('DB_PASSWORD','');
+    define('DB_NAME','listaprojetos');
+?>
