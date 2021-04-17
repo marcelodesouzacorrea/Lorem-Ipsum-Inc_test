0567485f0092789656182bd6c7204c4a712f5e2e
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-17 16:58:21 -0300

    alterei o nome do banco de dados

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ servidor com configuração padrão (usuário 'root' sem senha) -->
     define('DB_SERVER','localhost');
     define('DB_USER','root');
     define('DB_PASSWORD','');
-    define('DB_NAME','listaprojetos');
+    define('DB_NAME','projeto');
 
 // Tentativa de conexão com o banco de dados da aplicação 
     $link = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lorem-Ipsum-Inc</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="topo">
+            <h2>Lorem Ipsum Inc</h2>
+            <a href=""><button><i class="fa fa-plus"></i>Adicionar projeto</button></a>
+        </div>
+    </div>
+</body>
+</html>
