8c4e466aa12cd1542cfcfd3b0a7c9cc4513f8435
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 13:48:48 -0300

    inicializar com valores vazios

---
+++ update.php
@@ -0,0 +1,8 @@
+<?php
+    // inclusão do arquivo config.php
+    require_once "config.php";
+
+    // Definir variáveis ​​e inicializar com valores vazios
+    $nome = $inicio = $fim = $valor = $riscos = $participantes =  "";
+    $nome_err = $inicio_err = $fim_err = $valor_err = $riscos_err = $participantes_err = "";
+?>
