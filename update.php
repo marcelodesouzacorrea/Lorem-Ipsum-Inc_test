fc43284c7b5a1fcb67323def02d3ca8bc7252450
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 13:53:13 -0300

    valida  os dados

--- update.php
+++ update.php
@@ -9,4 +9,51 @@
     if(isset($_POST["id"]) && !empty($_POST["id"])){
         // Obter valor de entrada
         $id = $_POST["id"];
+
+        // Validar o nome
+        $input_nome = trim($_POST["nome"]);
+        if(empty($input_nome)){
+            $nome_err = "Digite o nome do projeto.";
+        } elseif(!filter_var($input_nome, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+            $nome_err = "Please enter a valid name.";
+        } else{
+            $nome = $input_nome;
+        }    
+        // Validar o inicio
+        $input_inicio = trim($_POST["inicio"]);
+        if(empty($input_inicio)){
+            $inicio_err = "Digite a data de inicio do projeto.";
+        } else{
+            $inicio = $input_inicio;
+        }    
+        // Validar o fim
+        $input_fim = trim($_POST["fim"]);
+        if(empty($input_fim)){
+            $fim_err = "Digite a data do fim do projeto.";
+        } else{
+            $fim = $input_fim;
+        }
+        // Validar o valor
+        $input_valor = trim($_POST["valor"]);
+        if(empty($input_valor)){
+            $valor_err = "Digite o valor do projeto.";
+        } elseif(!ctype_digit($input_valor)){
+            $valor_err = "Digite o valor do projeto.";
+        }else{
+            $valor = $input_valor;
+        }
+        // Validar riscos
+        $input_riscos = trim($_POST["riscos"]);
+        if(empty($input_riscos)){
+            $riscos_err = "Digite o nivel de riscos do projeto.";
+        } else{
+            $riscos = $input_riscos;
+        }  
+        // validar participantes
+        $input_participantes = trim($_POST["participantes"]);
+        if(empty($input_participantes)){
+            $participantes_err = "Digite o nome de participantes do projeto.";
+        } else{
+            $participantes = $input_participantes;
+        } 
 ?>
