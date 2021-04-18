0ce3e18ecda0feee336baf261cec1c1dc56f9501
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 13:50:15 -0300

    Processando dados do formulário

--- update.php
+++ update.php
@@ -5,4 +5,8 @@
     // Definir variáveis ​​e inicializar com valores vazios
     $nome = $inicio = $fim = $valor = $riscos = $participantes =  "";
     $nome_err = $inicio_err = $fim_err = $valor_err = $riscos_err = $participantes_err = "";
+    // Processando dados do formulário quando o formulário é enviado
+    if(isset($_POST["id"]) && !empty($_POST["id"])){
+        // Obter valor de entrada
+        $id = $_POST["id"];
 ?>
